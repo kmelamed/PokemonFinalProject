3d1cefd47e30e4dd81301d36f52188fdf3afae7f
kmelamed <kaylenmelamed@gmail.com>
2018-12-07 10:30:07 -0800

    Create Battle.java

---
+++ classes/Battle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Methods {
+
+	public static void main(String[] args) {
+	}
+	
+	public void Battle(Character c1, Character c2) {
+		Scanner in = new Scanner(System.in);
+		Pokemon trainer = trainer.getPokemonArray[0];
+		Pokemon enemy = c1.getPokemonArray[0];
+		
+		//p1 name
+		//hp
+		
+		//p2 name
+		//hp
+		
+		System.out.println("Enter a number 1-4 to select your move");
+		System.out.println("1. Attack\n2. Bag\n3. Pokemon\n4. Run");
+		int move = in.nextInt();
+		//user input to select move
+		
+		//attack, bag, pokemon, run	
+	}
+
+}
